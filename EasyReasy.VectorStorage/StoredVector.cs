a5a3b1249032da920e85772e84d94070279b3360
Adam Tovatt <sporedenstore@hotmail.com>
2025-08-05 13:31:37 +0200

    setup nuget package and add xml comments for stored vector

--- EasyReasy.VectorStorage/StoredVector.cs
+++ EasyReasy.VectorStorage/StoredVector.cs
@@ -1,8 +1,18 @@
 ﻿namespace EasyReasy.VectorStorage
 {
+    /// <summary>
+    /// Represents a vector stored in the vector store with its associated metadata.
+    /// </summary>
     public readonly struct StoredVector
     {
+        /// <summary>
+        /// Gets the unique identifier of the vector.
+        /// </summary>
         public readonly Guid Id;
+        
+        /// <summary>
+        /// Gets the vector values as a float array.
+        /// </summary>
         public readonly float[] Values;
 
         /// <summary>
@@ -11,6 +21,11 @@
         /// <returns>A read-only span over the vector values</returns>
         public ReadOnlySpan<float> GetSpan() => Values.AsSpan();
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StoredVector"/> struct with the specified ID and values.
+        /// </summary>
+        /// <param name="id">The unique identifier of the vector.</param>
+        /// <param name="values">The vector values.</param>
         public StoredVector(Guid id, float[] values)
         {
             Id = id;
